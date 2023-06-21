e789f519600f3d1f611ee83892ca4c2a97979c27
Joaquin Charovsky <joaquincharovsky@gmail.com>
2023-06-21 19:35:46 -0300

    merge-1

---
+++ pages/profile.tsx
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+const Profile = () => {
+  return (
+    <Typography align="center" variant="h2">
+      Profile View
+    </Typography>
+  );
+};
+
+export default Profile;
